7429d0647650178de89bbd7cc299c2a9ef6ffab7
Joe <joehoney1987@gmail.com>
2019-07-09 20:02:42 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,36 @@
-print("Hello World!")
+##Hopefully this script should run a simple coin toss game.##
+import random
+import time
+Heads = ("Heads")
+Tails = ("Tails")
+heads_and_tails = [(Heads),(Tails)]
+Result = random.choice(heads_and_tails)
+TimesPlayed = 0
+Score = 0
+ResultsList = []
+
+UserPlaying=input("Do you want to play a game? y/n \n")
+if UserPlaying == ("y"):
+    PlayTimes = int(input("OK, how many times do you want to play? \n"))
+else:
+    print("Thanks for your time, see you later!")
+
+while TimesPlayed < PlayTimes:
+    UserChoice=input("Pick Heads or Tails...\n")
+    time.sleep(1)
+    print(Result)
+    ResultsList.append(Result)
+    if Result == UserChoice:
+        Score += 10
+        print("Yay, you win! \n"
+        "Your score is now " ,Score,)
+    else:
+        Score -= 10
+        print("Better luck next time! \n"
+        "Your score is now ",Score,)
+    TimesPlayed = TimesPlayed + 1
+
+print("Thats great, thanks for playing! \n"
+      "Your final score is ",Score, "\n"
+      "It went down like ",ResultsList
+      )
